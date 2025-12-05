009dcf03ee61c64f80ee36f098febb2fda28cf54
daonq254 <dao.nq254@gmail.com>
2025-12-05 21:06:15 +0700

    20251205

---
+++ DTN2507_P6_AccountManagement/src/com/vti/backend/DepartmentManagement.java
@@ -0,0 +1,65 @@
+package com.vti.backend;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.vti.dao.DepartmentDao;
+import com.vti.entity.Department;
+import com.vti.utils.ScannerUtils;
+
+public class DepartmentManagement {
+
+	private DepartmentDao departmentDao;
+
+	public DepartmentManagement() throws FileNotFoundException, IOException {
+		super();
+		departmentDao = new DepartmentDao();
+	}
+
+	public void getAllDepartment() throws ClassNotFoundException, SQLException {
+		System.out.println("Hiển thị danh sách phòng ban trên hệ thống");
+
+		List<Department> lisDepartments = departmentDao.getAllDepartment();
+		String leftAlignFormat = "| %-6d | %-21s |%n";
+
+		System.out.format("+--------+-----------------------+%n");
+		System.out.format("|   ID   | Department Name       |%n");
+		System.out.format("+--------+-----------------------+%n");
+
+		for (Department department : lisDepartments) {
+			System.out.format(leftAlignFormat, department.getId(), department.getName());
+		}
+		System.out.format("+--------+-----------------------+%n");
+
+	}
+
+	public void getDepartmentById_5() throws ClassNotFoundException, SQLException {
+		System.out.println("Thông tin phòng ban số 5");
+		Department department_5 = departmentDao.getDepartmentById_5();
+		String leftAlignFormat = "| %-6d | %-21s |%n";
+
+		System.out.format("+--------+-----------------------+%n");
+		System.out.format("|   ID   | Department Name       |%n");
+		System.out.format("+--------+-----------------------+%n");
+		System.out.format(leftAlignFormat, department_5.getId(), department_5.getName());
+
+	}
+
+	public void getDepartmentById() throws ClassNotFoundException, SQLException {
+		System.out.println("Tìm phòng ban theo ID");
+		System.out.println("Mời bạn nhập vào ID của phòng ban cần tìm kiếm");
+		int idFind = ScannerUtils.inputIntPositive();
+
+		Department department = departmentDao.getDepartmentById(idFind);
+
+		String leftAlignFormat = "| %-6d | %-21s |%n";
+
+		System.out.format("+--------+-----------------------+%n");
+		System.out.format("|   ID   | Department Name       |%n");
+		System.out.format("+--------+-----------------------+%n");
+		System.out.format(leftAlignFormat, department.getId(), department.getName());
+	}
+
+}
---
+++ DTN2507_P6_AccountManagement/src/com/vti/dao/DepartmentDao.java
@@ -0,0 +1,74 @@
+package com.vti.dao;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.vti.entity.Department;
+import com.vti.utils.JdbcUtils;
+
+public class DepartmentDao {
+	private JdbcUtils jdbcUtils;
+
+	public DepartmentDao() throws FileNotFoundException, IOException {
+		super();
+		jdbcUtils = new JdbcUtils();
+	}
+
+	public List<Department> getAllDepartment() throws ClassNotFoundException, SQLException {
+		String sqlGetDepartment = "SELECT * FROM department ORDER BY DepartmentID";
+		ResultSet resultSet = jdbcUtils.executeQuery(sqlGetDepartment);
+
+		// Chuyển đổi Resutl Set sang List<Department>
+		List<Department> departments = new ArrayList();
+		while (resultSet.next()) {
+			Department department = new Department(resultSet.getInt("DepartmentID"),
+					resultSet.getString("DepartmentName"));
+			departments.add(department);
+
+		}
+		jdbcUtils.disConnection();
+		return departments;
+
+	}
+
+	public Department getDepartmentById_5() throws ClassNotFoundException, SQLException {
+		String sqlGetDepartment = "SELECT * FROM department WHERE DepartmentID = 5";
+		ResultSet resultSet = jdbcUtils.executeQuery(sqlGetDepartment); // tối đa 1 bản ghi
+
+		if (resultSet.next()) {
+			Department department = new Department(resultSet.getInt("DepartmentID"),
+					resultSet.getString("DepartmentName"));
+			jdbcUtils.disConnection();
+			return department;
+
+		}
+		jdbcUtils.disConnection();
+		return null;
+
+	}
+
+	public Department getDepartmentById(int idFind) throws ClassNotFoundException, SQLException {
+		String sqlGetDepartmentById = "SELECT * FROM department WHERE DepartmentID = ?";
+		PreparedStatement preparedStatement = jdbcUtils.createPrepareStatement(sqlGetDepartmentById);
+		preparedStatement.setInt(1, idFind);
+
+		ResultSet resultSet = preparedStatement.executeQuery();
+
+		if (resultSet.next()) {
+			Department department = new Department(resultSet.getInt("DepartmentID"),
+					resultSet.getString("DepartmentName"));
+			jdbcUtils.disConnection();
+			return department;
+
+		}
+		jdbcUtils.disConnection();
+		return null;
+
+	}
+
+}
